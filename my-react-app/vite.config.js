da865805c5b3c929a71017531c300b8b214a2dc7
Mitchel Bechtold <mcmitchel46@gmail.com>
2026-09-21 14:57:27 +0000

    Build React app for GitHub Pages

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -6,6 +6,7 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  base: "/IS2350webdevelopment/my-react-app/dist/", 
   server:{
     port: 5173, // Specify your desired port here
   }
